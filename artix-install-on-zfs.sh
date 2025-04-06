0e19521e070c8d71007b9d97e26d3b149dd5e47f
saretta1986 <saretta1986@proton.me>
2025-04-06 12:02:43 +0200

    fix: remove redundant swap partition activity check in EFI setup

--- artix-install-on-zfs.sh
+++ artix-install-on-zfs.sh
@@ -377,12 +377,6 @@ efiswap() {
         error "EFI partition is not mounted!"
     fi
 
-    # Check if the swap partition is active using UUID
-    swap_uuid=$(blkid -s UUID -o value ${DISK}-part3)
-    if ! swapon --show | grep -q "$swap_uuid"; then
-        error "Swap partition is not active!"
-    fi
-
     printf "%s\n" "${bold}EFI and swap partitions set up successfully!"
 }
 efiswap || error "Error setting up EFI and swap partitions!"
